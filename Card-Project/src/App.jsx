730e4b4e19c51cb1899b62e075198831649316ae
Akshay2415 <akshaygirnare1234@gmail.com>
2026-09-14 12:36:03 +0530

    Cards-Project using Components and Props

---
+++ Card-Project/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='parent'>
+      <div className="card">
+        
+        <div className="top">
+          <img src="" alt="" />
+          <button>Save</button>
+        </div>
+
+        <div className="centre">
+          
+        </div>
+
+        <div className="bottom">
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
