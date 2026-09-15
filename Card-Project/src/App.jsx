56269ef6683b297ebb0c30703267b483208d6cb2
Akshay2415 <akshaygirnare1234@gmail.com>
2026-09-15 17:41:35 +0530

    Project completed using props and components

--- Card-Project/src/App.jsx
+++ Card-Project/src/App.jsx
@@ -1,34 +1,156 @@
 import React from 'react'
-import {Bookmark} from 'lucide-react'
+import Cards from './components/Cards'
+
+const jobs = [
+  {
+    id: 1,
+    company: "Amazon",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/4/4a/Amazon_icon.svg",
+    posted: "5 days ago",
+    title: "Senior UI/UX Developer",
+    type: "Part Time",
+    level: "Senior Level",
+    salary: "$120/hr",
+    location: "Mumbai, India",
+  },
+  {
+    id: 2,
+    company: "Google",
+    logo: "https://www.google.com/favicon.ico",
+    posted: "2 days ago",
+    title: "Frontend Developer",
+    type: "Full Time",
+    level: "Mid Level",
+    salary: "$110/hr",
+    location: "Bangalore, India",
+  },
+  {
+    id: 3,
+    company: "Microsoft",
+    logo: "https://www.microsoft.com/favicon.ico",
+    posted: "1 week ago",
+    title: "React Developer",
+    type: "Full Time",
+    level: "Senior Level",
+    salary: "$125/hr",
+    location: "Hyderabad, India",
+  },
+  {
+    id: 4,
+    company: "Meta",
+    logo: "https://www.facebook.com/favicon.ico",
+    posted: "3 days ago",
+    title: "UI/UX Designer",
+    type: "Part Time",
+    level: "Mid Level",
+    salary: "$100/hr",
+    location: "Mumbai, India",
+  },
+  {
+    id: 5,
+    company: "Netflix",
+    logo: "https://www.netflix.com/favicon.ico",
+    posted: "4 days ago",
+    title: "Product Designer",
+    type: "Full Time",
+    level: "Senior Level",
+    salary: "$130/hr",
+    location: "Remote",
+  },
+  {
+    id: 6,
+    company: "Apple",
+    logo: "https://www.apple.com/favicon.ico",
+    posted: "6 days ago",
+    title: "Frontend Engineer",
+    type: "Full Time",
+    level: "Senior Level",
+    salary: "$140/hr",
+    location: "Bangalore, India",
+  },
+  {
+    id: 7,
+    company: "Adobe",
+    logo: "https://www.adobe.com/favicon.ico",
+    posted: "1 day ago",
+    title: "UX Researcher",
+    type: "Part Time",
+    level: "Junior Level",
+    salary: "$90/hr",
+    location: "Noida, India",
+  },
+  {
+    id: 8,
+    company: "Spotify",
+    logo: "https://www.spotify.com/favicon.ico",
+    posted: "2 weeks ago",
+    title: "Web Developer",
+    type: "Full Time",
+    level: "Mid Level",
+    salary: "$115/hr",
+    location: "Remote",
+  },
+  {
+    id: 9,
+    company: "Flipkart",
+    logo: "https://www.flipkart.com/favicon.ico",
+    posted: "3 days ago",
+    title: "Software Developer",
+    type: "Full Time",
+    level: "Junior Level",
+    salary: "$80/hr",
+    location: "Bangalore, India",
+  },
+  {
+    id: 10,
+    company: "NVIDIA",
+    logo: "https://www.nvidia.com/favicon.ico",
+    posted: "2 days ago",
+    title: "Frontend Software Engineer",
+    type: "Full Time",
+    level: "Senior Level",
+    salary: "$135/hr",
+    location: "Bangalore, India",
+  },
+  {
+    id: 11,
+    company: "Tesla",
+    logo: "https://www.tesla.com/favicon.ico",
+    posted: "4 days ago",
+    title: "Software Engineer",
+    type: "Full Time",
+    level: "Mid Level",
+    salary: "$125/hr",
+    location: "Pune, India",
+  },
+  {
+    id: 12,
+    company: "Uber",
+    logo: "https://www.uber.com/favicon.ico",
+    posted: "1 week ago",
+    title: "Senior React Developer",
+    type: "Full Time",
+    level: "Senior Level",
+    salary: "$120/hr",
+    location: "Bangalore, India",
+  }
+];
 
 const App = () => {
   return (
     <div className='parent'>
-      <div className="card">        
-          <div>
-            <div className="top">
-              <img src="https://upload.wikimedia.org/wikipedia/commons/4/4a/Amazon_icon.svg" alt="" />
-              <button>Save <Bookmark size={12} /></button>
-            </div>
-
-            <div className="centre">
-              <h3>Amazon <span>5 days ago</span></h3>
-              <h2>Senior UI/UX Developer</h2>
-              <div className='tag'>
-                <h4>Part Time</h4>
-                <h4>Senior Level</h4>
-              </div>
-            </div>
-          </div>
-
-            <div className="bottom">
-              <div>
-                <h3>$120/hr</h3>
-                <p>Mumbai,India</p>
-              </div>
-              <button>Apply Now</button>
-            </div>
-      </div>
+      {jobs.map(function(elem,id){
+        return <div key={id}>
+            <Cards company={elem.company}
+                  logo = {elem.logo}
+                  posted = {elem.posted}
+                  type = {elem.type}
+                  title = {elem.title}
+                  level = {elem.level}
+                  salary= {elem.salary}
+                  location = {elem.location}/>
+        </div>
+      })}
     </div>
   )
 }
